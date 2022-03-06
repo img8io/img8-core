2e68233bdea81bb4ced654bfd8ff9ded4a5035d5
Victor F <nftvictor@gmail.com>
2022-03-06 08:38:37 +0800

    Add more features into the protocol with some code refactors.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // ThrottlerModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => ({
+    //     ttl: config.get('THROTTLE_TTL'),
+    //     limit: config.get('THROTTLE_LIMIT'),
+    //   }),
+    // }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 50
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    AppService,
+  ]
 })
 export class AppModule {}
